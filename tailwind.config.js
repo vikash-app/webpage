41d9e9b15eb4d5d7d9d0fd31fcfa805758bddaea
vikash-app <vikashk7@icloud.com>
2025-06-12 22:47:23 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        pink: {
+          50: '#ffe4ed',
+          100: '#ffb8d2',
+          500: '#ff385c',
+          600: '#e11d48',
+          700: '#be185d',
+        },
+        cyan: {
+          50: '#ecfeff',
+          100: '#cffafe',
+          400: '#22d3ee',
+          500: '#06b6d4',
+          700: '#0e7490',
+        },
+      },
+    },
+  },
+}
+
