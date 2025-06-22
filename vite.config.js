1bc22cabbda7689afeb9d7e457ace7685f12e3bd
vikash-app <vikashk7@icloud.com>
2025-06-22 23:27:59 +0200

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/webpage/', // <-- Add this line, use your repo name!
+  base: '/', // <-- Add this line, use your repo name!
   plugins: [react()],
 })
